0dac40ea2b907fdfed775bea51bdad67e0854eda
Cleyton Ferrari <cleyton@w7br.com>
2013-08-24 19:09:16 -0400

    adiciona token em cookie

--- APIExemplo/UI.Api/App_Start/BundleConfig.cs
+++ APIExemplo/UI.Api/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace UI.Api
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquerycookie").Include(
+                       "~/Scripts/jquery.cookie.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
